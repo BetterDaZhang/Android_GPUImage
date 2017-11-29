8af22b83732b2b6ad5ad84177551f0c4ecaa3c55
Junling <junling.zhang@camdora.me>
2017-11-29 16:59:40 +0800

    finish

---
+++ app/src/main/java/com/zhangjunling/example/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.zhangjunling.example.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.zhangjunling.example.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
